9d2dae377fa3e423728ef8d29c5ebb8da4c4957a
jnnsrctr <johannes.richter@nordakademie.de>
2023-02-04 17:53:08 +0100

    First logging stuff

---
+++ NewFolder/ReportManager.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualBasic;
+using NLog;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace WebAutomator.ReportHelper;
+
+
+public class ReportManager
+{
+    private static readonly object padlock = new();
+    private static ReportManager _instance;
+    private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+    public ReportManager()
+    {
+
+    }
+    public static ReportManager Instance
+    {
+        get
+        {
+            lock (padlock)
+            {
+                if (_instance == null)
+                    _instance = new ReportManager();
+            }
+            return _instance;
+        }
+    }
+
+    public void Log(string logLevel, string message)
+    {
+        switch (logLevel)
+        {
+            case "Trace":
+                _logger.Trace(message);
+                break;
+            case "Debug":
+                _logger.Debug(message);
+                break;
+            case "Info":
+                _logger.Info(message);
+                break;
+            case "Warn":
+                _logger.Warn(message);
+                break;
+            case "Error":
+                _logger.Error(message);
+                break;
+            case "Fatal":
+                _logger.Fatal(message);
+                break;
+            default:
+                _logger.Info(message);
+                break;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NLog;
+using WebAutomator.ReportHelper;
+
+public class Program
+{
+    private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+    public static int Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        ReportManager.Instance.Log("Trace", "WebAutomator starts");
+        ReportManager.Instance.Log("Debug", "WebAutomator starts");
+        ReportManager.Instance.Log("Warn", "WebAutomator starts");
+        ReportManager.Instance.Log("Error", "WebAutomator starts");
+        ReportManager.Instance.Log("Fatal", "WebAutomator starts");
+        ReportManager.Instance.Log("Info", "WebAutomator starts");
+        return 0;
+    }
+}
